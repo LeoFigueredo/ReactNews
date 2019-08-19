45730e04917e11ac9c15e76e2e0fa9703b5c0b18
Leonardo Figueredo <Leonardo.Figueredo@techandsolve.com>
2019-08-18 22:00:03 -0500

    Creando el Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment} from 'react';
+import Header from './Components/Header'; 
 
 class App extends Component {
+state = {
+  noticias: []
+}
+componentDidMount(){
+ this.consultarNoticias();
+}
+consultarNoticias = async () =>{
+  const url = `https://newsapi.org/v2/top-headlines?country=co&category=business&apiKey=16e84bcacb254f83bdd95663952d61df`;
+
+  const respuesta = await fetch(url);
+  const noticias = await respuesta.json();
+
+  /*this.state({
+    noticias: noticias.articles
+  })*/
+
+}
+
   render() {
     return (
-     <h1>Noticias Api React</h1>
+     <Fragment>
+       <Header
+        titulo = 'Noticias React API'
+       />
+       <div className="container white contenedor-noticias"></div>
+     </Fragment>
     );
   }
 }
